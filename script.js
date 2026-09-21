2a3f2c40c7d534e81af4dd6a7ddf8a383c25c80d
Priyam Patra <priyampatra63@gmail.com>
2026-09-21 19:48:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const accessKey = "pEy3LTmaJeSF9DVISRj2INidCxCzfMMeirASTCbm1Ts";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data)
+
+  if (page === 1) {
+    searchResult.innerHTML = "";
+  }
+
+  const results = data.results;
+  results.map((results) => {
+    const image = document.createElement("img");
+    image.src = results.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = results.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+  showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = Math.floor(Math.random() * 50) + 1;
+  searchResult.innerHTML = "";
+  searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
+  searchImages();
+});
